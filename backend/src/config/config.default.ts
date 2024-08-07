70d9f1114d6ad36c0c62a2d327b1f4696cca60bb
khdfffk <3079821586@qq.com>
2024-08-07 23:07:13 +0800

    change the structure

--- backend/src/config/config.default.ts
+++ backend/src/config/config.default.ts
@@ -2,7 +2,7 @@ import { MidwayConfig } from '@midwayjs/core';
 
 export default {
   // use for cookie sign key, should change to your own and keep security
-  keys: '1722655865776_8242',
+  keys: '1723043061115_2419',
   koa: {
     port: 7001,
   },
